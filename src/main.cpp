dc9c065552fcc69368847d15abedd0e2a6d3799b
Nayeem <naye2m@outlook.com>
2026-04-09 00:56:34 +0600

    UPDT: Remove gzip header from root handler response in main loop

--- src/main.cpp
+++ src/main.cpp
@@ -148,7 +148,7 @@ void loop() {
 
 // Root handler
 void handleRoot() {
-  server.sendHeader("Content-Encoding", "gzip");
+  // server.sendHeader("Content-Encoding", "gzip");
   // server.send(200, "text/html", homeHTML, sizeof(homeHTML));
   server.send(200, "text/html", homeHTML);
   cleanDisplay();
