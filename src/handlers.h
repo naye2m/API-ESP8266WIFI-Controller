5a55a91405d3f53a3051e417b5e0462d507bdff3
Nayeem <naye2m@outlook.com>
2026-04-09 00:31:21 +0600

    ADD: Create handlers header file with function declarations for ESP8266 WiFi controller

---
+++ src/handlers.h
@@ -0,0 +1,15 @@
+// handlers.h
+#pragma once
+
+void handleRoot();
+void cleanDisplay();
+void handleLEDOn();
+void handleLEDOff();
+void handleLEDBlink();
+void handleLEDBrightness();
+void handleDisplay();
+void handlePixel();
+void handleShape();
+void animateText();
+void handle2DArray();
+void clearBeforeRender();
